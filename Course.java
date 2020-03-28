7e507cbaea5499a57d19af982fd4a3457dd4808d
viktorijapetkovska <viktorija.petkovska@students.finki.ukim.mk>
2020-03-28 19:15:26 +0100

    Modify

---
+++ Course.java
@@ -0,0 +1,15 @@
+class Course{
+	List<Student> students = new ArrayList<Student>();
+
+
+	public double getAverage() {
+		//TODO
+		Student sum = 0
+		for (Student s : students) {
+			sum+=s.getLabPoints();	
+				}
+	}
+
+	return sum/s.labPoints.size();
+
+}
