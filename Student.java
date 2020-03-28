f105be3546f29963f8153689d8badba38cff7ecd
viktorijapetkovska <viktorija.petkovska@students.finki.ukim.mk>
2020-03-28 18:59:16 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -5,14 +5,60 @@ class Student {
 	List<Integer> labPoints = new ArrayList<Integer>();
 
 	//TODO constructor
+	public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
 
 	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
 
 	public double getAverage() {
 		//TODO
+		Integer sum = 0
+		for (Integer i : labPoints) {
+			sum+=i.getLabPoints;	
+				}
 	}
+
+	return sum/labPoints.size();
 
 	public boolean hasSignature() {
 		//TODO
+		if (labPoints.size()>=8)
+			return true;
+		else return false;
 	}
 }
